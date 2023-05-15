fc8850d762f931a20cd7aca1ef867f37f2017474
Alen Komic <akomic@users.noreply.github.com>
2023-05-15 17:27:47 +0200

    Adding update-kubeconfig

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,13 @@ if [ ! -d "$HOME/.kube" ]; then
     mkdir -p $HOME/.kube
 fi
 
+export KUBECONFIG=$HOME/.kube/config
+
+
+if [ ! -z $EKS_NAME ];then
+  aws eks update-kubeconfig --region $AWS_REGION --name $EKS_NAME
+fi
+
 echo "/usr/local/bin/kubectl" >> $GITHUB_PATH
 echo "/usr/local/bin/aws" >> $GITHUB_PATH
 
